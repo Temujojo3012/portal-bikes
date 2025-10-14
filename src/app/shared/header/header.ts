cd011b5d9be1b77eaaa21b52d1dfeb2d314a0d9a
Temujojo3012 <faundezramon92@gmail.com>
2025-10-13 21:41:34 -0300

    añadido estilo global body container

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -3,9 +3,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../services/auth.service';
+import { RouterLink } from "@angular/router";
+import { MatRippleModule } from '@angular/material/core';
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, MatRippleModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
